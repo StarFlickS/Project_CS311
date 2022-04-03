c579e09865503529c16ce20332d02b9bb9e6f63f
starflickS <purin1746@gmail.com>
2022-04-03 20:15:29 +0700

    Pull From remote

--- ProjectCS311.py
+++ ProjectCS311.py
@@ -90,10 +90,7 @@ def LoginPage(root):
     Button(bottom_frm, text="Forget password?", fg="black", font="verdana 15 bold", bg="white", borderless=1, command=ForgetPasswordPage).grid(row=1, column=1)
 
     gmail_ent.focus_force()
-<<<<<<< HEAD
-=======
 
->>>>>>> aee798329db785e39a6b7173842990bfe00df12b
 
 def RegistrationPage():
     global rg_frm, fname_ent, lname_ent, gmail_ent, phone_ent, pwd_ent, cfpwd_ent
@@ -415,21 +412,12 @@ def menufame(result):
     mf_frm.columnconfigure((0,1,2),weight=1)
     mf_frm.grid(row=0, rowspan=2, column=0,sticky="news",padx=10, pady=150)
     
-<<<<<<< HEAD
-    Button(mf_frm,text="My country",fg="#D6E5FA",bg="#808cff",image=img_home,compound=LEFT, command=ReportCovid19THPage).grid(row=0,column=0,columnspan=2,sticky="w",pady=20,ipady=40,padx=20,ipadx=20)
-    Button(mf_frm,text="All",fg="#D6E5FA",bg="#808cff",image=img_earth,compound=LEFT).grid(row=0,column=1,sticky="w",pady=20,ipady=40,padx=20,ipadx=20)
-    Button(mf_frm,text="list",fg="#D6E5FA",bg="#808cff",image=img_lupa,compound=LEFT).grid(row=0,column=2,columnspan=2,sticky="e",pady=20,ipady=40,padx=10,ipadx=20)
-    Button(mf_frm,text="Station",fg="#D6E5FA",bg="#808cff",image=img_map,compound=LEFT).grid(row=1,column=0,columnspan=2,sticky="w",pady=20,ipady=40,padx=20,ipadx=20)
-    Button(mf_frm,text="Chatbot",fg="#D6E5FA",bg="#808cff",image=img_bot,compound=LEFT).grid(row=1,column=1,sticky="w",pady=20,ipady=40,padx=20,ipadx=20)
-    Button(mf_frm,text="Log out",fg="#D6E5FA",bg="#808cff",image=img_out,command=logout,compound=LEFT).grid(row=1,column=2,columnspan=2,sticky="e",pady=20,ipady=40,padx=10,ipadx=20)
-=======
     Button(mf_frm,text="My country",fg="#D6E5FA",bg="#808cff",font="verdana 14",image=img_home,compound=LEFT, command=ReportCovid19THPage, borderless=1).grid(row=0,column=0, sticky="news", pady=20, padx=20)
     Button(mf_frm,text="All",fg="#D6E5FA",bg="#808cff",font="verdana 15",image=img_earth,compound=LEFT, borderless=1).grid(row=0,column=1, sticky="news", pady=20)
     Button(mf_frm,text="list",fg="#D6E5FA",bg="#808cff",font="verdana 15",image=img_lupa,compound=LEFT, borderless=1).grid(row=0,column=2, sticky="news", padx=20, pady=20)
     Button(mf_frm,text="Station",fg="#D6E5FA",bg="#808cff",font="verdana 14",image=img_map,compound=LEFT, borderless=1).grid(row=1,column=0, sticky="news", padx=20, pady=10)
     Button(mf_frm,text="Chatbot",fg="#D6E5FA",bg="#808cff",font="verdana 14",image=img_bot,compound=LEFT, borderless=1).grid(row=1,column=1, sticky="news", pady=10)
     Button(mf_frm,text="Log out",fg="#D6E5FA",bg="#808cff",font="verdana 14",image=img_out,command=logout,compound=LEFT, borderless=1).grid(row=1,column=2, sticky="news", padx=20, pady=10)
->>>>>>> aee798329db785e39a6b7173842990bfe00df12b
 
 
 def logout(): 
